a4a13c0904481ae88d9fb3f77d75519b683edb6b
darren-cook <darren.j.cook@uconn.edu>
2021-12-29 22:51:06 -0500

    updated styling for containers. Added functions to displaybook and update hidden elements.

--- script.js
+++ script.js
@@ -1,4 +1,20 @@
+const tableFull = document.querySelector("#bookTable");
+const newBookForm = document.querySelector("#formContainer");
+
+const addButton = document.querySelector("#addButton");
+const submitButton = document.querySelector("#submitButton")
+
+const newBookTitleForm = document.querySelector("#newBookTitle");
+const newBookAuthorForm = document.querySelector("#newBookAuthor");
+const newBookPagesForm = document.querySelector("#newBookPages");
+const newBookReadForm = document.querySelector("#newBookRead");
+const newBookRatingForm = document.querySelector("#newBookRating");
+
+
 let myLibrary = [];
+
+addBookToLibrary("Sample Title", "Sample Author", "xxx", "Not Read", "x/5")
+displayLibrary(myLibrary);
 
 function Book(title, author, pages, read, rating) {
     this.title = title;
@@ -17,7 +33,6 @@ function addBookToLibrary(title, author, pages, read, rating){
 }
 
 function displayLibrary(libraryArray) {
-    const tableFull = document.querySelector("#bookTable");
     libraryArray.forEach(function(item){
         const tableRow = document.createElement("tr");
 
@@ -43,8 +58,68 @@ function displayLibrary(libraryArray) {
     })
 }
 
-addBookToLibrary("The Hobbit", "Tolkien", "349", "Not Read", "4.5")
-addBookToLibrary("LOTR", "JRR", "849", "Read", "3.0")
-displayLibrary(myLibrary)
-console.log(myLibrary)
+function displayBook(libraryArrayIndex){
+    const tableRow = document.createElement("tr");
+
+    const titleData = document.createElement("td");
+    const authorData = document.createElement("td");
+    const pagesData = document.createElement("td");
+    const readData = document.createElement("td");
+    const ratingData = document.createElement("td");
+
+    titleData.textContent = libraryArrayIndex.title;
+    authorData.textContent = libraryArrayIndex.author;
+    pagesData.textContent = libraryArrayIndex.pages;
+    readData.textContent = libraryArrayIndex.read;
+    ratingData.textContent = libraryArrayIndex.rating;
+
+    tableRow.appendChild(titleData);
+    tableRow.appendChild(authorData);
+    tableRow.appendChild(pagesData);
+    tableRow.appendChild(readData);
+    tableRow.appendChild(ratingData);
+
+    tableFull.appendChild(tableRow)
+}
+
+addButton.addEventListener("click", function(){
+    displaySubmitForm();
+})
+
+submitButton.addEventListener("click", function(){
+    hideSubmitForm();
+    gatherData();
+    clearData();
+    displayBook(myLibrary[myLibrary.length-1]);
+})
+
+function displaySubmitForm() {
+    newBookForm.style.display = "flex";
+    addButton.style.display = "none";
+    submitButton.style.display = "inline-block"
+}
+
+function hideSubmitForm() {
+    newBookForm.style.display = "none";
+    addButton.style.display = "inline-block";
+    submitButton.style.display = "none"
+}
+
+function gatherData() {
+    const newBookTitleData = newBookTitleForm.value;
+    const newBookAuthorData = newBookAuthorForm.value;
+    const newBookPagesData = newBookPagesForm.value;
+    const newBookReadData = newBookReadForm.value;
+    const newBookRatingData = newBookRatingForm.value;
+
+    addBookToLibrary(newBookTitleData, newBookAuthorData, newBookPagesData, newBookReadData, newBookRatingData);
+}
+
+function clearData() {
+    newBookTitleForm.value = "";
+    newBookAuthorForm.value = "";
+    newBookPagesForm.value = "";
+    newBookReadForm.value = "";
+    newBookRatingForm.value = "";
+}
 
